5a8abf6fb36d3255c9058c94de6f97540b396b0f
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-06-21 14:46:33 +0100

    added input box, list to hold added items and a working delete button

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,43 @@ const App = () => {
     setInputValue(e.target.value);
   };
 
-  const HandleAddToList = () => {
-    if (inputValue.trim() !== "") { // Ensures the input is not empty or just whitespace
+  const handleAddToList = () => {
+    if (inputValue.trim() !== "") {
+      // Ensures the input is not empty or just whitespace
       // Updates `toDo` using `setToDo` with the new array that includes the new object
-      setToDo([...toDo, { id: toDo.length + 1, text: inputValue }]); 
+      setToDo([...toDo, { id: toDo.length + 1, text: inputValue }]);
       setInputValue(""); // Clears `inputValue` by making it an empty string
     }
   };
 
   // The `filter` method is used to create a new array `updatedToDo` that excludes the item with the specified `id`
   const handleDeleteToDo = (id) => {
-    const updatedToDo = toDo.filter((todo) => todo.id !== id)
-    setToDo(updatedToDo)
-  }
+    const updatedToDo = toDo.filter((todo) => todo.id !== id);
+    setToDo(updatedToDo);
+  };
 
-  return <h1>To Do List</h1>;
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <div className="toDoInput">
+        <input 
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="To do..."
+        />
+        <button onClick={handleAddToList}>Add to List</button>
+      </div>
+      <ul className="toDoList">
+        {toDo.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={()=> handleDeleteToDo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
